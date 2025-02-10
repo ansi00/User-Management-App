043617b84ae5bda259ebaabb17f108ea9cf31884
Anas Abbas <anasabbasmalik88@gmail.com>
2025-02-10 15:08:03 +0500

    Login Ui and functionality using gerasim api

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginObj: any = {
+    EmailId: '',
+    Password: '',
+  };
+  userService = inject(UserService);
+  router = inject(Router);
 
+  Login() {
+    this.userService.onLogin(this.loginObj).subscribe((res: any) => {
+      if (res.result) {
+        localStorage.setItem('userApp', JSON.stringify(res.data));
+        this.router.navigateByUrl('user-list');
+      } else {
+        alert(res.message);
+      }
+    });
+  }
 }
